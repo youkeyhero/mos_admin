a61e56a4ff180ede301754a663511b0dbea3d7bc
youkeyhero <yukihiro.kondo@mulodo.com>
2020-10-27 07:46:41 +0000

    set cancancan

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  #20201026
+  enum role: { admin: 1, member: 2 }
+
 end
